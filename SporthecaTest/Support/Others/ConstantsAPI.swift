3207af00c3043125bd87a58a29e80bac32fe4d69
Patricia Souza <p.souza@e-ai.com.br>
2022-11-24 15:24:38 -0300

    fixing lint

--- SporthecaTest/Support/Others/ConstantsAPI.swift
+++ SporthecaTest/Support/Others/ConstantsAPI.swift
@@ -5,5 +5,5 @@
 //  Created by Patricia Cintia de Souza on 24/11/22.
 //
 
-let baseUrl : String = "http://sportsmatch.com.br/teste"
-let homeExtensionURL : String = "/teste.json"
+let baseUrl: String = "http://sportsmatch.com.br/teste"
+let homeExtensionURL: String = "/teste.json"
